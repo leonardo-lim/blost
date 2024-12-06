994b7dc329d01c49cb7b5185f51efca3064ca47c
leonardo-lim <leonardolim178@gmail.com>
2024-12-06 12:50:34 +0700

    chore: add tanstack query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Provider from '@/components/provider/Provider';
 import Layout from '@/components/layout/Layout';
+
+const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <Provider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
+            <QueryClientProvider client={queryClient}>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </QueryClientProvider>
         </Provider>
     );
 };
