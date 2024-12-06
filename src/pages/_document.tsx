3e2b5b771779fffaca03e20723c7469316ab9caa
leonardo-lim <leonardolim178@gmail.com>
2024-12-06 23:08:13 +0700

    fix: linting error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+
 import type { DocumentContext } from 'next/document';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
