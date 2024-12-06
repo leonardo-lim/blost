3b9265177b59aef046ce81bc054824b1940e0291
leonardo-lim <leonardolim178@gmail.com>
2024-12-06 14:57:07 +0700

    feat: add pagination support for getting posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,26 +1,39 @@
 import type { NextPage } from 'next';
-import type { PostType } from '@/types/post-type';
+import type { PostsQueryType, PostType } from '@/types/post-type';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { Alert, Col, Flex, Row, Typography } from 'antd';
+import { Alert, Col, Flex, Pagination, Row, Typography } from 'antd';
 import { getPosts } from '@/services/post-service';
+import { getAccessToken } from '@/utils/auth';
 import SearchInput from '@/components/post/SearchInput';
 import PostCard from '@/components/post/PostCard';
 
 const Posts: NextPage = () => {
-    const accessToken = '';
+    const [currentPageNumber, setCurrentPageNumber] = useState(1);
+    const [postsTotal, setPostsTotal] = useState(0);
 
-    const { data: posts, isLoading, isError } = useQuery<PostType[]>({
-        queryKey: ['posts', accessToken],
-        queryFn: () => getPosts(accessToken)
+    const { data: posts, isLoading, isError } = useQuery<PostsQueryType>({
+        queryKey: ['posts', currentPageNumber],
+        queryFn: () => getPosts(getAccessToken(), currentPageNumber)
     });
 
     const [filteredPosts, setFilteredPosts] = useState<PostType[]>([]);
 
+    const handleChangePageNumber = (page: number) => {
+        setCurrentPageNumber(page);
+    };
+
     useEffect(() => {
         if (posts) {
-            setFilteredPosts(posts);
+            setFilteredPosts(posts.data);
+
+            const paginationTotal = posts.headers['x-pagination-total'];
+
+            if (paginationTotal) {
+                const total = parseInt(paginationTotal.toString());
+                setPostsTotal(total);
+            }
         }
     }, [posts]);
 
@@ -30,7 +43,7 @@ const Posts: NextPage = () => {
                 <title>Blost | Posts</title>
             </Head>
 
-            <Flex className="pt-24 px-10 md:px-20">
+            <Flex vertical className="gap-8 px-10 md:px-20 py-24">
                 {isLoading && (
                     <Typography.Title level={5}>Getting posts...</Typography.Title>
                 )}
@@ -43,18 +56,33 @@ const Posts: NextPage = () => {
                     />
                 )}
                 {posts && (
-                    <Flex vertical className="w-full gap-6">
-                        <SearchInput posts={posts} setFilteredPosts={setFilteredPosts} />
-                        <Row gutter={[20, 20]}>
-                            {filteredPosts.map((post, idx) => {
-                                return (
-                                    <Col key={idx} xs={24} md={12} xl={8}>
-                                        <PostCard post={post} />
-                                    </Col>
-                                );
-                            })}
-                        </Row>
-                    </Flex>
+                    <>
+                        <Flex vertical className="w-full gap-6">
+                            {posts.data.length > 0 && (
+                                <SearchInput
+                                    posts={posts.data}
+                                    setFilteredPosts={setFilteredPosts}
+                                />
+                            )}
+                            <Row gutter={[20, 20]}>
+                                {filteredPosts.map((post, idx) => {
+                                    return (
+                                        <Col key={idx} xs={24} md={12} xl={8}>
+                                            <PostCard post={post} />
+                                        </Col>
+                                    );
+                                })}
+                            </Row>
+                        </Flex>
+                        <Flex justify="center">
+                            <Pagination
+                                current={currentPageNumber}
+                                pageSize={10}
+                                total={postsTotal}
+                                onChange={handleChangePageNumber}
+                            />
+                        </Flex>
+                    </>
                 )}
             </Flex>
         </>
--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -1,14 +1,17 @@
-import type { PostType } from '@/types/post-type';
+import type { PostsQueryType } from '@/types/post-type';
 import axiosInstance from '@/lib/axios-instance';
 
-const getPosts = async (accessToken: string): Promise<PostType[]> => {
-    const response = await axiosInstance.get('/users/7567035/posts', {
+const getPosts = async (accessToken: string, page: number): Promise<PostsQueryType> => {
+    const response = await axiosInstance.get(`/users/7567035/posts?page=${page}&per_page=10`, {
         headers: {
             Authorization: `Bearer ${accessToken}`
         }
     });
 
-    return response.data;
+    return {
+        data: response.data,
+        headers: response.headers
+    };
 };
 
 export { getPosts };
